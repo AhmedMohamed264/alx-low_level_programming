1ad31ec53da9f4902eba2892fd401107b2f0dc38
AhmedMohamed264 <medo.mohamed201958@gmail.com>
2023-03-17 15:15:52 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ n *= -1;
 n %= 10;
 
 if (n > 5)
-printf("%d and is greater than 5\n", n);
+printf("%d and is greater than 5\n", n * ((n > 5) ? 1 : -1));
 else if (n == 0)
 printf("%d and is 0\n", n);
 else
-printf("%d and is less than 6 and not 0\n", n);
+printf("%d and is less than 6 and not 0\n", n * ((n > 5) ? 1 : -1));
 
 return (0);
 }
