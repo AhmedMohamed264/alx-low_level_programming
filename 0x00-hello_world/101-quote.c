9c07f1d1ebff07819f50b1ced5c83f91bd636e8d
AhmedMohamed264 <medo.mohamed201958@gmail.com>
2023-03-16 17:47:49 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+        /* Write MESSAGE to the standard error */
+        write(2, MESSAGE, sizeof(MESSAGE) - 1);
+
+        /* Return 1 to indicate an error occurred */
+        return (1);
+}
